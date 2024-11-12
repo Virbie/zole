6acec458e2e2419e1603d86758e2421ce1ac4b4e
Virbie <149624389+Virbie@users.noreply.github.com>
2024-11-12 16:56:57 +0200

    Add files via upload

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+const path = require('path');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+io.on('connection', (socket) => {
+  console.log('New client connected');
+
+  socket.on('joinRoom', (roomCode) => {
+    socket.join(roomCode);
+    console.log(`Client joined room: ${roomCode}`);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('Client disconnected');
+  });
+});
+
+const PORT = process.env.PORT || 4000;
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
