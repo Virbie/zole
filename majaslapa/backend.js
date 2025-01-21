f67e0ef209b2daa416299c65b00e6fd1b559b228
RicardoTheLostOne <yellowfantasyhouse@gmail.com>
2025-01-21 13:45:24 +0200

    Update backend.js

--- majaslapa/backend.js
+++ majaslapa/backend.js
@@ -5,13 +5,6 @@ let player1 = []
     let kartis = [];
 
 function Karsudalisana(){
-
-    
-
-    document.getElementById("Visaskartis").innerHTML = kartis.toString();
-    document.getElementById("Player1kartis").innerHTML = player1.toString();
-    document.getElementById("Player2kartis").innerHTML = player2.toString();
-    document.getElementById("Player3kartis").innerHTML = player3.toString();
     let test = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26]
     
    
