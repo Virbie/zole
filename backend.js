7212b8634d43a57bf71e93ff7f6b91c473371f71
RicardoTheLostOne <yellowfantasyhouse@gmail.com>
2025-01-28 14:49:04 +0200

    Izdariju izmainas karsu dalisanas koda.
    Paslaik nebija laika lai ieliktu so kodu lielaja majaslapa. Velak to izdarot, izdzesisu so failu.

---
+++ backend.js
@@ -0,0 +1,78 @@
+    let playerGajienaID = 1  // Gajiens apzime vai paslaik ir tavs gajiens
+
+    let trumpe = [13,14,15,16,17,18,19,20,21,22,23,24,25,26]
+    let kreicis = [1,2,3,4]
+    let pikis = [5,6,7,8]
+    let sirsnina = [9,10,11,12]
+
+    const arrays = [kreicis, pikis, sirsnina, trumpe];
+    
+    let foundArray = 0;
+
+function Bots(value){
+    console.log(value)
+
+    for (let i = 0; i < arrays.length; i++) {
+        if (arrays[i].includes(value)) { // atrod masivu un karts veidu kas jaliek visiem cilvekiem
+            foundArray = arrays[i]; // Track the name of the array
+            console.log(foundArray);
+            break;
+        }
+    }
+
+
+}
+
+
+
+
+    // Sākums karsu dalisanai
+    let player1 = []
+    let player2 = []
+    let player3 = []
+    let galds = []
+    let kartis = [];
+
+function Karsudalisana(){
+    let test = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26]
+    
+   
+    let removeElement = (array, n) => {
+    
+    let newArray = [];
+    for (let i = 0; i < array.length; i++) {
+    
+    if (array[i] !== array[n]) {
+    newArray.push(array[i]);
+    }
+    
+    }
+    return newArray;
+    };
+    
+    
+    for(i = 0; i < 26;i++){
+    
+    let x = Math.floor(Math.random()*(26-i))
+    
+    kartis[i] = test[x]
+    
+    test = removeElement(test, x);
+    
+    }
+    
+    for(i = 0; i< 26;i++){
+        if(i<8){player1[i] = kartis[i]}
+        else if(i<16){player2[i-8] = kartis[i]}
+        else if(i<24){player3[i-16] = kartis[i]}
+        else{galds[i-24] = kartis[i]}
+    }
+    console.log(player1, player2, player3,galds)
+    
+    
+    
+    return kartis
+    
+}
+// beigas karsu dalisanai
+
