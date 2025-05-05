1d6ec17f0acecb9be5720018c0823277a57baf1b
Virbie <149624389+Virbie@users.noreply.github.com>
2025-05-05 05:30:00 +0300

    kip noteikumi uzrakstiti utt, baigi vel jauns nekas nav bet kautkas nedaudz ir

--- cardgame-server/public/include_footer.js
+++ cardgame-server/public/include_footer.js
@@ -231,4 +231,12 @@ document.addEventListener("DOMContentLoaded", () => {
   initRaveSwitch();
   initLightModeSwitch();
   initspecialSwitch();
+  fetch('footer.html')
+    .then(response => response.text())
+    .then(html => {
+      document.getElementById('footer').innerHTML = html;
+    })
+    .catch(error => {
+      console.error('Kaut kas nogāja greizi ar footeru:', error);
+    });
 });
